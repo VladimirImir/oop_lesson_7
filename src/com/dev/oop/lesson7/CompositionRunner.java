31a314d8bb58b46e6a700eafa9c3d235fc76cdf6
VladimirImir <mimimvvv@gmail.com>
2023-06-01 19:32:53 +0300

    8.1 - Stack and Heap. Theory.

--- src/com/dev/oop/lesson7/CompositionRunner.java
+++ src/com/dev/oop/lesson7/CompositionRunner.java
@@ -1,6 +1,9 @@
 package com.dev.oop.lesson7;
 
 public class CompositionRunner {
+    
+    /** 8.1 Stack and Heap. Теория */
+
     public static void main(String[] args) {
         Ram ram = new Ram(1024);
         Ssd ssd = new Ssd(512);
